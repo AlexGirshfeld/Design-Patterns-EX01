cf216f336f6fcb9c3b1b70a79479be90a630ffa8
Tal Goldstein <talg@jfrog.com>
2019-12-02 18:30:50 +0200

    Added PostDataAggregator and InfluenceAnalyser classes

---
+++ InfluencerToolkit/InfluencerToolkit/InfluenceAnalyser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using FacebookWrapper.ObjectModel;
+
+namespace InfluencerToolkit
+{
+    public class InfluenceAnalyser
+    {
+        private User m_user;
+        private SortedList<User, int> m_UsersSortedByLikesCount;
+
+        public InfluenceAnalyser(User i_user)
+        {
+            this.m_user = i_user;
+            this.m_UsersSortedByLikesCount = new PostsDataAggregator(this.m_user).UsersSortedByLikes();
+        }
+
+        public int AnalysePostInfluenceLevel(Post i_post)
+        {
+            int postInfluenceLevel = 0;
+            if (this.m_user.Posts.Contains(i_post))
+            {
+
+            } else
+            {
+                throw new Exception("The post you requested was not found!");
+            }
+            return postInfluenceLevel;
+        }
+
+    }
+}
