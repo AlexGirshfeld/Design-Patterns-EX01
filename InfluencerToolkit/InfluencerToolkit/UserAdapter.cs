eb19ca24080fe3aed77fe6632636a3643b971acd
Alex Girshfeld <alex.girshfeld@gmail.com>
2020-01-08 13:10:49 +0200

    more Adapterchanges

--- InfluencerToolkit/InfluencerToolkit/UserAdapter.cs
+++ InfluencerToolkit/InfluencerToolkit/UserAdapter.cs
@@ -3,30 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FacebookWrapper.ObjectModel;
+using System.Drawing;
+using System.Net;
+
 namespace InfluencerToolkit
 {
-    public class UserAdapter
+    public class UserAdapter : IFacebookObjAdapter
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Image ProfilePicture { get; set; }
         public List<PostAdapter> Posts { get; set; }
-        //public List<AlbumsAdapter> Albums { get; set; }
-        //public List<FriendsAdapter> Friends { get; set; }
-    }
-    public UserAdapter()
-    {
+        public List<AlbumAdapter> Albums { get; set; }
+        public List<UserAdapter> Friends { get; set; }
         
-    }
+       
 
-    public void AdaptUser(User i_UserToAdapt)
-    {
-        this Posts = adaptPosts(i_UserToAdapt.Posts);
-        this.Albums = adaptCollection<AdaptedAlbum,Album>(i_UserToAdapt.Albums);
-        this.Friends = adaptCollection<UserAdapter,User>(i_UserToAdapt.Friends
-    }
-    
-    public List<T1> AdaptCollection<T1,T2> (FacebookObjectCollection<T2> i_CollectionToAdapt)
-    {
-        List<T1> retList = new List<T1>();
 
-        return retList;
+        public UserAdapter()
+        {
+        }
+
+        public void AdaptFacebookObj(FacebookObject i_UserToAdapt)
+        {
+            if (i_UserToAdapt is User)
+            {
+                this.Posts = CollectionAdapter.AdaptCollection<PostAdapter,Post>((i_UserToAdapt as User).Posts);
+                this.Albums = CollectionAdapter.AdaptCollection<AlbumAdapter, Album>((i_UserToAdapt as User).Albums);
+                this.Friends = CollectionAdapter.AdaptCollection<UserAdapter, User>((i_UserToAdapt as User).Friends);
+                this.FirstName = (i_UserToAdapt as User).FirstName;
+                this.LastName = (i_UserToAdapt as User).LastName;
+                this.ProfilePicture = (i_UserToAdapt as User).ImageNormal;
+            }
+            else
+            {
+                throw new Exception("trying to adapt into a User Adapter an object that is not a Facebook User");
+            }
+        }
+
+        private Image LoadImageFromUrl(string URL)
+        {
+            Image retImage = null;
+            var request = WebRequest.Create(URL);
+
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                retImage = Bitmap.FromStream(stream);
+            }
+            retImage;
+        }
+
+
     }
 }
